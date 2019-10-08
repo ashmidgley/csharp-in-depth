d5251090c6c1bf835f371cf5d134b33ee5973663
Ash Midgley <ayelgdom@tutanota.com>
2019-10-08 12:01:20 +0100

    Minor feature examples.

---
+++ App/C#-2/PragmaDirectives.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace App
+{
+    // Pragma directives give extra info to the compiler.
+    public static class PragmaDirectives
+    {
+        public static void ExampleUsage()
+        {
+            // CS0219 is the warning for unused variables.
+            #pragma warning disable CS0219
+            var x = "Unused variable.";
+            Console.WriteLine("Haha try and complain now you silly compiler.");
+            #pragma warning restore CS0219
+        }
+    }
+}
