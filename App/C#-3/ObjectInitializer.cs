fe42a3876a587c4782451584939057d310707f31
Ash Midgley <ayelgdom@tutanota.com>
2019-10-08 18:45:45 +0100

    Object/collection intitializer and anonymous types examples.

---
+++ App/C#-3/ObjectInitializer.cs
@@ -0,0 +1,23 @@
+namespace App
+{
+    public static class ObjectInitializer
+    {
+        public struct Order {
+            public int CustomerId;
+            public int ProductId;
+        }
+
+        public static void ExampleUsage()
+        {
+            Order orderOld = new Order();
+            orderOld.CustomerId = 1;
+            orderOld.ProductId = 1;
+
+            var orderNew = new Order 
+            {
+                CustomerId = 1,
+                ProductId = 1
+            };
+        }
+    }
+}
